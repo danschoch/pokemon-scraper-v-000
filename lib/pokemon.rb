880ae76e0dbbbd3682ad5a40d37a3e3df5453bde
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 20:27:10 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,8 @@ class Pokemon
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
 
+    binding.pry
+
     #self.new(id: pk_id[0], name: pk_name, type: pk_type, db: db_connect)
   end
 end
