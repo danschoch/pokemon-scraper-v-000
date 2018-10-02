7fa7289a54fac87225c3175468098c3ace3e0327
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 20:24:22 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,13 +12,13 @@ class Pokemon
     db_connect.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
-  def self.find (id_num, db_connect)
+  def self.find (id_num, db)
     #pk_id = db_connect.execute("SELECT id FROM pokemon WHERE id = ?", id_num)
     #pk_name = db_connect.execute("SELECT name FROM pokemon WHERE id = ?", id_num)
     #pk_type = db_connect.execute("SELECT type FROM pokemon WHERE id = ?", id_num)
 
-    pokemon_info = db_connect.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db_connect)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
 
     #self.new(id: pk_id[0], name: pk_name, type: pk_type, db: db_connect)
   end
