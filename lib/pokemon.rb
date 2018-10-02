0d1543194920dfde5e82d9247892e6e7ae7e1d75
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 20:32:31 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,14 +13,8 @@ class Pokemon
   end
 
   def self.find (id_num, db_connect)
-    #pk_id = db_connect.execute("SELECT id FROM pokemon WHERE id = ?", id_num)
     pk_name = db_connect.execute("SELECT name FROM pokemon WHERE id = ?", id_num).flatten
     pk_type = db_connect.execute("SELECT type FROM pokemon WHERE id = ?", id_num).flatten
-
-    #pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    #Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
-
-    #binding.pry
 
     self.new(id: id_num, name: pk_name[0], type: pk_type[0], db: db_connect)
   end
